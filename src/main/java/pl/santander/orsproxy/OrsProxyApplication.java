dec249c519dc94d4c53169ec4140f28b4274f324
jurant <jurant91@gmail.com>
2023-03-24 09:05:30 +0100

    Add files via upload

---
+++ src/main/java/pl/santander/orsproxy/OrsProxyApplication.java
@@ -0,0 +1,13 @@
+package pl.santander.orsproxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrsProxyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrsProxyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/santander/orsproxy/OrsProxyApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.santander.orsproxy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrsProxyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
